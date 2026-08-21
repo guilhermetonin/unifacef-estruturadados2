b51cdaaffdacafd66e339a986bd43e36c95b8b53
Guilherme <gtoninjorge@gmail.com>
2026-08-21 10:05:30 -0300

    fix: implementação de algoritmo de ordenação Selection Sort

--- 07-selection-sort.mjs
+++ 07-selection-sort.mjs
@@ -1,33 +1,35 @@
+import { nomes } from './data/nomes-desord.mjs'
+
+// metricas de desempenho
 let pass, comps, trocas
 
 function selectionSort(vetor) {
-    pass = 0, comps = 0, trocas = 0
+  pass = 0, comps = 0, trocas = 0
 
-    for (let posSel = 0; posSel < vetor.length - 1; posSel++) {
-        pass++
-        let posMenor = posSel + 1
+  for (let posSel = 0; posSel < vetor.length - 1; posSel++) {
+    pass++
+    let posMenor = posSel + 1 // assume o proximo num como menor
 
-        for (let i = posMenor + 1; i < vetor.length; i++) {
-            if (vetor[posMenor] > vetor[i]) {
-                posMenor = i
-                comps++
-            }
-        }
+    // busca o menor elemento no restante do vetor
+    for (let i = posMenor + 1; i < vetor.length; i++) {
+      comps++
 
-        comps++
-        if (vetor[posSel] > vetor[posMenor]) {
-            [ vetor[posSel], vetor[posMenor] ] = [ vetor[posMenor], vetor[posSel] ]
-            trocas++
-        }
+      if (vetor[posMenor] > vetor[i]) {
+        posMenor = i // atualiza o index com o menor num encontrado
+
+      }
     }
-    console.log(`pass: ${pass}, comps: ${comps}, trocas: ${trocas}`)
+
+    comps++
+    if (vetor[posSel] > vetor[posMenor]) {
+
+      [vetor[posSel], vetor[posMenor]] = [vetor[posMenor], vetor[posSel]]
+      trocas++
+    }
+  }
+
+  console.log(`pass: ${pass}, comps: ${comps}, trocas: ${trocas}`)
 }
-// let numeros = [77,44,22,33,99,55,88,0,66,11]
-// console.log("\nNumeros: " + numeros)
 
-// selectionSort(numeros)
-// console.log("\nDepois: " + numeros)
-
-import { nomes } from './data/nomes-desord.mjs'
 selectionSort(nomes)
 console.log(nomes)
--- 08-selection-sort-obj.mjs
+++ 08-selection-sort-obj.mjs
@@ -1,28 +1,37 @@
+import { objMotoristas } from './data/motoristas-obj-desord.mjs'
+
 let pass, comps, trocas
 
 function selectionSort(vetor, fnComp) {
-    pass = 0; comps = 0; trocas = 0
+	pass = 0; comps = 0; trocas = 0
 
-    for (let posSel = 0; posSel < vetor.length - 1; posSel++) {
-        pass++
-        let posMenor = posSel + 1
+	for (let posSel = 0; posSel < vetor.length - 1; posSel++) {
+		pass++
+		let posMenor = posSel + 1
 
-        for (let i = posMenor + 1; i < vetor.length; i++) {
-            if (vetor[posMenor] > vetor[i]) {
-                posMenor = i
-                comps++
-            }
-        }
+		for (let i = posMenor + 1; i < vetor.length; i++) {
+			comps++
 
-        comps++
-        if (fnComp( vetor[posSel], vetor[posMenor] ) {
-            [ vetor[posSel], vetor[posMenor] ] = [ vetor[posMenor], vetor[posSel] ]
-            trocas++
-        }
-    }
-    console.log(`pass: ${pass}, comps: ${comps}, trocas: ${trocas}`)
+			if (fnComp(vetor[posMenor], vetor[i])) {
+				posMenor = i
+
+			}
+		}
+
+		comps++
+		if (fnComp(vetor[posSel], vetor[posMenor])) {
+			[vetor[posSel], vetor[posMenor]] = [vetor[posMenor], vetor[posSel]]
+			trocas++
+
+		}
+	}
+
+	console.log(`pass: ${pass}, comps: ${comps}, trocas: ${trocas}`)
 }
 
-import {objMotoristas} from './data/motoristas-obj-desord.mjs'
+// vetor, funcao de comparacao
+selectionSort(objMotoristas, (elem1, elem2) => 
+	elem1.nome_motorista > elem2.nome_motorista
+)
 
-selectionSort(O)
+console.log(objMotoristas)
